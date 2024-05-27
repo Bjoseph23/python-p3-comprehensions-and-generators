e7be93d6ebf5589d7907ba23cef9eeabd33e623c
Bill Mageni <+>
2024-05-27 20:58:09 +0300

    Done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_int=[n for n in num_list if n % 2==0]
+    return even_int
+
 
 def make_exclamation(sentence_list):
-    pass
+    exclamated=[st + "!" for st in sentence_list ]
+    return exclamated
+
+result= return_evens([0,1,3,5,7,8,9,10])
+print(result)
+
+string_list =  make_exclamation(["Hello", "World"])
+print(string_list )
